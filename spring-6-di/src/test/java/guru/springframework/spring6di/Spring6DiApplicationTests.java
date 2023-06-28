bf5f09cfde28e7cf7d73306d6dd218ed77cf43cd
Claudiu-Dan Balint <cbalint@deloittece.com>
2023-06-28 08:51:55 +0300

    spring-di: spring context

--- spring-6-di/src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ spring-6-di/src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,33 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		assertEquals("Hello Everyone!!!", myController.sayHello());
+	}
+
+	@Test
+	void testAutowiredController() {
+		assertEquals("Hello Everyone!!!", myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
