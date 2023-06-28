4d14117436a0f7dec7cef8759e203cc539773089
Claudiu-Dan Balint <cbalint@deloittece.com>
2023-06-28 10:43:58 +0300

    spring-di: dependency injection without Spring

---
+++ spring-6-di/src/main/java/guru/springframework/spring6di/controllers/PropertyInjectedController.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PropertyInjectedController {
+
+    GreetingService greetingService;
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ spring-6-di/src/test/java/guru/springframework/spring6di/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void sayHello() {
+        String greeting = controller.sayHello();
+        System.out.println(greeting);
+
+        assertEquals("Hello Everyone from the Base Service!", greeting);
+    }
+}
